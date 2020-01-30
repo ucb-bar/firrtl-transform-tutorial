11ee3fe85ccea2bb9dbd076900fc66fb63230d05
Albert Magyar <albert.magyar@gmail.com>
2020-01-30 11:38:04 -0800

    Add annotation-handing and rename scaffolding to ResilientRegisters

--- src/main/scala/gcd/tools/ResilientRegisters.scala
+++ src/main/scala/gcd/tools/ResilientRegisters.scala
@@ -17,7 +17,54 @@ object ResilientRegisters extends Transform {
   val inputForm = LowForm
   val outputForm = LowForm
 
+  type TripleCopyNameMap = Map[String, Tuple3[String, String, String]]
+
+  private def renameReg(reg: DefRegister, newName: String): DefRegister = {
+    val renamed = reg.copy(name = newName)
+    if (renamed.reset == UIntLiteral(0)) {
+      renamed.copy(init = WRef(renamed))
+    }  else {
+      renamed
+    }
+  }
+
+  private def makeRegTriples(copyNameMap: TripleCopyNameMap)(stmt: Statement): Statement = {
+    stmt
+  }
+
+  private def regTripleNames(ns: Namespace, regName: String): (String, String, String) = {
+    (ns.newName(s"${regName}_copy_0"), ns.newName(s"${regName}_copy_1"), ns.newName(s"${regName}_copy_2"))
+  }
+
   def execute(state: CircuitState): CircuitState = {
-    state
+    val resilientRegTargets = state.annotations.collect {
+      case ResilientRegisterAnnotation(target) =>
+        require(target.isLocal)
+        target
+    }
+
+    val resilientRegsByModule = resilientRegTargets.groupBy(_.module)
+
+    // We need to record that we've split each register into three new ones
+    val renames = RenameMap()
+
+    val transformedModules = state.circuit.modules.map {
+      case m: DefModule if (resilientRegsByModule.contains(m.name)) =>
+        val ns = Namespace(m)
+        val resilientRegs = resilientRegsByModule(m.name)
+        val copyMap = resilientRegs.map(rTarget => rTarget -> regTripleNames(ns, rTarget.ref)).toMap
+        val copyNameMap = copyMap.map { case (k, v) => k.ref -> v }
+        copyMap.foreach {
+          case (oldTarget, (copyName0, copyName1, copyName2)) =>
+            val newTargets = Seq(copyName0, copyName1, copyName2).map(name => oldTarget.copy(ref = name))
+            renames.record(oldTarget, newTargets)
+        }
+        m.map(makeRegTriples(copyNameMap))
+      case m => m
+    }
+
+    val transformedCircuit = state.circuit.copy(modules = transformedModules)
+    val filteredAnnos = state.annotations.filterNot(_.isInstanceOf[ResilientRegisterAnnotation])
+    state.copy(transformedCircuit, outputForm, filteredAnnos, Some(renames))
   }
 }
